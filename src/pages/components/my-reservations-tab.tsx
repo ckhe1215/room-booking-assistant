92d6d7e33aaaa2fa447dc77a20a0ff663ada3dea
ckhe1215 <ckhe1215@gmail.com>
2026-02-23 21:22:34 +0900

    feat: 예약 목록 출력

--- src/pages/components/my-reservations-tab.tsx
+++ src/pages/components/my-reservations-tab.tsx
@@ -1,34 +1,37 @@
+import { Calendar } from "@/components/ui/calendar";
+import { getMyReservationsQueryOptions } from "@/src/remotes/queryOptions";
+import { SuspenseQuery } from "@suspensive/react-query";
 import { ReservationCard } from "./reservation-card";
 
 export function MyReservationsTab() {
   return (
-    <div className="space-y-4">
-      {/* 
-      // 예약 내역이 없을 때 표시할 컴포넌트
-      <div className="text-muted-foreground py-20 text-center">
-        <Calendar className="mx-auto mb-4 h-12 w-12 opacity-50" />
-        <p>예약 내역이 없습니다.</p>
-        <p className="mt-2 text-xs">예약 조회/취소 로직을 구현해보세요.</p>
-      </div>
-      */}
-      <ReservationCard
-        name="회의실 1"
-        date="2026-01-18"
-        startTime="10:00"
-        endTime="11:00"
-        capacity={4}
-        equipments={["tv"]}
-        onCancel={() => {}}
-      />
-      <ReservationCard
-        name="회의실 2"
-        date="2026-01-18"
-        startTime="10:00"
-        endTime="11:00"
-        capacity={4}
-        equipments={["tv", "whiteboard"]}
-        onCancel={() => {}}
-      />
-    </div>
+    <SuspenseQuery {...getMyReservationsQueryOptions()}>
+      {({ data }) => {
+        if (data.length === 0) { // TODO: 테스트
+          return <div className="text-muted-foreground py-20 text-center">
+            <Calendar className="mx-auto mb-4 h-12 w-12 opacity-50" />
+            <p>예약 내역이 없습니다.</p>
+            <p className="mt-2 text-xs">예약 조회/취소 로직을 구현해보세요.</p>
+          </div>
+        }
+        return (
+          <div className="space-y-4">
+            {
+              data.map((item) => <> <ReservationCard
+                name={item.roomId}
+                date={item.date}
+                startTime={item.start}
+                endTime={item.end}
+                capacity={item.attendees}
+                equipments={item.equipments}
+                onCancel={() => { }}
+              /></>)
+            }
+          </div>
+        )
+
+      }}
+    </SuspenseQuery >
+
   );
 }
