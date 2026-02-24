b6df12e68d5ec1ebee40debaa315e646b7458027
ckhe1215 <ckhe1215@gmail.com>
2026-02-24 09:31:06 +0900

    refactor: 예약 폼 리팩토링

--- src/pages/components/booking-tab.tsx
+++ src/pages/components/booking-tab.tsx
@@ -12,7 +12,7 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { toast } from "@/hooks/use-toast";
 import { createReservations, getReservationsQueryOptions, getRoomsQueryOptions, RoomsResponse } from "@/src/remotes/queryOptions";
 import { SuspenseQueries, SuspenseQuery } from '@suspensive/react-query';
-import { useQueryClient } from "@tanstack/react-query";
+import { QueryClient, useQueryClient } from "@tanstack/react-query";
 import { format, isAfter, parse } from "date-fns";
 import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -28,28 +28,30 @@ type BookingFormValues = {
   equipments: ('tv' | 'whiteboard' | 'video' | 'speaker')[];
 }
 
+const DEFAULT_FORM_VALUES: BookingFormValues = {
+  date: new Date(),
+  attendees: 1,
+  start: "9:00",
+  end: "10:00",
+  equipments: [],
+}
+
 export function BookingTab() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const dateParam = searchParams.get("date") || format(new Date(), 'yyyy-MM-dd');
-  const { control, register, watch, handleSubmit, formState: { errors }, trigger } = useForm<BookingFormValues>({
-    mode: "onChange",
-    defaultValues: {
-      date: new Date(),
-      attendees: 1,
-      start: "9:00",
-      end: "10:00",
-      equipments: [],
-    },
-  });
-  const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
   const queryClient = useQueryClient();
 
-  const startTime = watch("start");
-  const formDate = watch("date");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dateParam = searchParams.get("date") || format(new Date(), 'yyyy-MM-dd');
+
+  const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
+
+  const { control, register, watch, handleSubmit, formState: { errors }, trigger } = useForm<BookingFormValues>({
+    mode: "onChange",
+    defaultValues: DEFAULT_FORM_VALUES,
+  });
 
   useEffect(() => {
     trigger("end");
-  }, [startTime, trigger]);
+  }, [watch("start"), trigger]);
 
   return (
     <div className="space-y-6">
@@ -85,7 +87,7 @@ export function BookingTab() {
         </CardContent>
       </Card>
 
-      <form onSubmit={handleSubmit(async (data) => {
+      <form id="reservation-form" onSubmit={handleSubmit((data) => {
         if (!selectedRoom) {
           toast({
             title: "예약 실패",
@@ -93,30 +95,14 @@ export function BookingTab() {
           });
           return;
         }
-
-        if (isAfter(parse(data.start, "HH:mm", new Date()), parse(data.end, "HH:mm", new Date()))) {
+        if (validateStartEndTime(data.start, data.end)) {
           toast({
             title: "예약 실패",
             description: "종료 시간이 시작 시간보다 늦어야합니다.",
           });
           return;
         }
-        const res = await createReservations({
-          roomId: selectedRoom,
-          date: format(data.date, 'yyyy-MM-dd'),
-          start: data.start,
-          end: data.end,
-          attendees: data.attendees,
-          equipments: data.equipments,
-        });
-
-        if (res.ok) {
-          queryClient.invalidateQueries({ queryKey: ["reservations", format(formDate, "yyyy-MM-dd")] });
-          toast({
-            title: "예약 완료",
-            description: "예약이 완료되었습니다.",
-          });
-        }
+        handleCreateReservation(data, selectedRoom, queryClient);
       })}>
         <Card>
           <CardHeader>
@@ -145,12 +131,7 @@ export function BookingTab() {
               name="end"
               control={control}
               rules={{
-                validate: (value) => {
-                  const start = parse(watch("start"), "HH:mm", new Date());
-                  const end = parse(value, "HH:mm", new Date());
-
-                  return isAfter(end, start) || "종료 시간은 시작 시간 이후이어야 합니다.";
-                }
+                validate: (value) => !validateStartEndTime(watch("start"), value) || "종료 시간은 시작 시간 이후이어야 합니다."
               }}
               render={({ field }) => (
                 <SelectField label="종료 시간" options={getTimeOptions({ from: 9, to: 20 })}
@@ -159,27 +140,24 @@ export function BookingTab() {
                 />
               )}
             />
-            {errors.end && <p className="text-red-500">종료 시간은 시작 시간 이후이어야 합니다.</p>}
+            {errors.end && <p className="text-red-500">{errors.end.message}</p>}
             <Controller
               name="floor"
               control={control}
               render={({ field }) => (
                 <SuspenseQuery {...getRoomsQueryOptions()}>
-                  {({ data }) => {
-                    return (
-                      <SelectField
-                        label="선호 층 (선택)"
-                        value={field.value}
-                        onValueChange={field.onChange}
-                        options={getFloors(data)}
-                      />
-                    )
-                  }}
+                  {({ data }) =>
+                    <SelectField
+                      label="선호 층 (선택)"
+                      value={field.value}
+                      onValueChange={field.onChange}
+                      options={getFloors(data)}
+                    />
+                  }
                 </SuspenseQuery>
 
               )}
             />
-
             <div className="space-y-2">
               <Label>필요 장비</Label>
               <Controller
@@ -187,66 +165,62 @@ export function BookingTab() {
                 control={control}
                 render={({ field }) => (
                   <ToggleGroup type="multiple" variant="outline" spacing={2} size="sm" value={field.value} onValueChange={field.onChange}>
-                    <ToggleGroupItem value="tv">
-                      <Tv className="h-4 w-4" />
-                      TV
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value="whiteboard">
-                      <Presentation className="h-4 w-4" />
-                      화이트보드
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value="video">
-                      <Video className="h-4 w-4" />
-                      화상회의
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value="speaker">
-                      <Volume2 className="h-4 w-4" />
-                      스피커
-                    </ToggleGroupItem>
+                    {[
+                      { label: "TV", value: "tv", icon: <Tv className="h-4 w-4" /> },
+                      { label: "화이트보드", value: "whiteboard", icon: <Presentation className="h-4 w-4" /> },
+                      { label: "화상회의", value: "video", icon: <Video className="h-4 w-4" /> },
+                      { label: "스피커", value: "speaker", icon: <Volume2 className="h-4 w-4" /> }
+                    ].map((item) => (
+                      <ToggleGroupItem key={item.value} value={item.value}>
+                        {item.icon}
+                        {item.label}
+                      </ToggleGroupItem>
+                    ))}
                   </ToggleGroup>
                 )}
               />
             </div>
           </CardContent>
         </Card>
-
-
-        <Card>
-          <CardHeader>
-            <CardTitle>예약 가능한 회의실</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <SuspenseQueries queries={[getRoomsQueryOptions(), getReservationsQueryOptions(format(formDate, "yyyy-MM-dd"))]}>
-              {([{ data: rooms }, { data: reservations }]) => {
-                const filteredRooms = rooms.filter((room) => {
-                  return room.capacity >= watch("attendees");
-                }).filter((room) => {
-                  const requiredEquipments = watch("equipments");
-                  return requiredEquipments.every((equipment) => room.equipments.includes(equipment));
-                }).filter((room) => {
-                  const preferredFloor = watch("floor");
-                  return preferredFloor ? room.floor === Number(preferredFloor) : true;
-                });
-                const availableRooms = filteredRooms.filter((room) => {
-                  const start = parse(watch("start"), "HH:mm", new Date());
-                  const end = parse(watch("end"), "HH:mm", new Date());
-                  return !reservations.some((reservation) => reservation.roomId === room.id && isAfter(end, parse(reservation.start, "HH:mm", new Date())) && isAfter(parse(reservation.end, "HH:mm", new Date()), start));
-                });
-
-                return <>
-                  {availableRooms.map((room) => (
-                    <RoomSelect key={room.id} selected={room.id === selectedRoom} onSelect={() => {
-                      setSelectedRoom(room.id);
-                    }} name={room.name} floor={room.floor} capacity={room.capacity} equipments={room.equipments} />
-                  ))}
-                </>
-              }}
-            </SuspenseQueries>
-
-            <Button size="lg" type="submit">예약하기</Button>
-          </CardContent>
-        </Card>
       </form>
+
+
+
+      <Card>
+        <CardHeader>
+          <CardTitle>예약 가능한 회의실</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SuspenseQueries queries={[getRoomsQueryOptions(), getReservationsQueryOptions(format(watch("date"), "yyyy-MM-dd"))]}>
+            {([{ data: rooms }, { data: reservations }]) => {
+              const filteredRooms = rooms.filter((room) => {
+                return room.capacity >= watch("attendees");
+              }).filter((room) => {
+                const requiredEquipments = watch("equipments");
+                return requiredEquipments.every((equipment) => room.equipments.includes(equipment));
+              }).filter((room) => {
+                const preferredFloor = watch("floor");
+                return preferredFloor ? room.floor === Number(preferredFloor) : true;
+              });
+              const availableRooms = filteredRooms.filter((room) => {
+                const start = parse(watch("start"), "HH:mm", new Date());
+                const end = parse(watch("end"), "HH:mm", new Date());
+                return !reservations.some((reservation) => reservation.roomId === room.id && isAfter(end, parse(reservation.start, "HH:mm", new Date())) && isAfter(parse(reservation.end, "HH:mm", new Date()), start));
+              });
+
+              return <>
+                {availableRooms.map((room) => (
+                  <RoomSelect key={room.id} selected={room.id === selectedRoom} onSelect={() => {
+                    setSelectedRoom(room.id);
+                  }} name={room.name} floor={room.floor} capacity={room.capacity} equipments={room.equipments} />
+                ))}
+              </>
+            }}
+          </SuspenseQueries>
+
+          <Button size="lg" type="submit" form="reservation-form">예약하기</Button>
+        </CardContent>
+      </Card>
 
     </div >
   );
@@ -270,4 +244,29 @@ export const getTimeOptions = ({ from, to }: { from: number; to: number }) => {
     label: `${to}:00`,
     value: `${to}:00`,
   });
+}
+
+const handleCreateReservation = async (data: BookingFormValues, selectedRoom: string, queryClient: QueryClient) => {
+  const res = await createReservations({
+    roomId: selectedRoom,
+    date: format(data.date, 'yyyy-MM-dd'),
+    start: data.start,
+    end: data.end,
+    attendees: data.attendees,
+    equipments: data.equipments,
+  });
+
+  if (res.ok) {
+    queryClient.invalidateQueries({ queryKey: ["reservations", format(data.date, "yyyy-MM-dd")] });
+    toast({
+      title: "예약 완료",
+      description: "예약이 완료되었습니다.",
+    });
+  }
+}
+
+const validateStartEndTime = (start: string, end: string) => {
+  const startTime = parse(start, "HH:mm", new Date());
+  const endTime = parse(end, "HH:mm", new Date());
+  return isAfter(startTime, endTime);
 }
